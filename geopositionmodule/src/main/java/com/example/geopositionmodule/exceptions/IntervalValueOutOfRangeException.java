bab634b32a95d2ef16530ea506f035ebffa21603
01gaM <olga.molchanova.olkamana@gmail.com>
2021-05-01 00:45:47 +0300

    Fix IntervalValueOutOfRangeException message format

--- geopositionmodule/src/main/java/com/example/geopositionmodule/exceptions/IntervalValueOutOfRangeException.java
+++ geopositionmodule/src/main/java/com/example/geopositionmodule/exceptions/IntervalValueOutOfRangeException.java
@@ -6,11 +6,11 @@ import java.util.Locale;
 
 import androidx.annotation.Nullable;
 
-public class IntervalValueOutOfRangeException extends Exception{
+public class IntervalValueOutOfRangeException extends Exception {
     @Nullable
     @Override
     public String getMessage() {
-        return String.format(Locale.US, "Значение интервала должно быть от %.2f до %d мин",
+        return String.format(Locale.US, "Значение интервала должно быть от %.3f до %.0f мин",
                 LocationProvider.MINIMUM_UPDATE_INTERVAL,
                 LocationProvider.MAXIMUM_UPDATE_INTERVAL);
     }
